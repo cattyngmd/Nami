1abbc6ad82b11895630dd2dad3d86b80f5694b16
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-06-09 10:15:31 +0200

    hud update shitcodefix

--- src/main/java/me/kiriyaga/essentials/feature/module/impl/client/HUDModule.java
+++ src/main/java/me/kiriyaga/essentials/feature/module/impl/client/HUDModule.java
@@ -3,11 +3,12 @@ package me.kiriyaga.essentials.feature.module.impl.client;
 import me.kiriyaga.essentials.event.EventPriority;
 import me.kiriyaga.essentials.event.SubscribeEvent;
 import me.kiriyaga.essentials.event.impl.Render2DEvent;
+import me.kiriyaga.essentials.event.impl.UpdateEvent;
 import me.kiriyaga.essentials.feature.module.Category;
 import me.kiriyaga.essentials.feature.module.Module;
 import me.kiriyaga.essentials.setting.impl.BoolSetting;
+import me.kiriyaga.essentials.setting.impl.IntSetting;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.MathHelper;
 
@@ -25,11 +26,60 @@ public class HUDModule extends Module {
     public final BoolSetting cords = addSetting(new BoolSetting("Coordinates", false));
     public final BoolSetting facing = addSetting(new BoolSetting("Facing", true));
     public final BoolSetting FPS = addSetting(new BoolSetting("FPS", true));
+    public final IntSetting updateInterval = addSetting(new IntSetting("Update Interval", 5, 1, 20));
 
     private static final int PADDING = 5;
 
+    private int tickCounter = 0;
+
+    // since updating each tick (or even each render tick) means updating 20/60 times per second, we better timeout theese interactions
+    private String watermarkText = "";
+    private String coordsText = "";
+    private String facingText = "";
+    private String fpsText = "";
+    private String pingText = "";
+
     public HUDModule() {
-        super("HUD","Displays in game hud.", Category.CLIENT, "ргв");
+        super("HUD", "Displays in game hud.", Category.CLIENT, "ргв");
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onUpdate(UpdateEvent event) {
+        tickCounter++;
+        if (tickCounter % updateInterval.get() != 0) return;
+
+        if (MINECRAFT.world == null || MINECRAFT.player == null)
+            return;
+
+        if (watermark.get()) {
+            watermarkText = NAME + " " + VERSION;
+        } else {
+            watermarkText = null;
+        }
+
+        if (cords.get()) {
+            coordsText = "XYZ " + Formatting.WHITE +
+                    formatCoord(MINECRAFT.player.getX()) + Formatting.GRAY + ", " + Formatting.WHITE +
+                    formatCoord(MINECRAFT.player.getY()) + Formatting.GRAY + ", " + Formatting.WHITE +
+                    formatCoord(MINECRAFT.player.getZ());
+        } else {
+            coordsText = null;
+        }
+
+        if (facing.get()) {
+            facingText = getFacing(MINECRAFT);
+        } else {
+            facingText = null;
+        }
+
+        if (FPS.get()) {
+            fpsText = "FPS: " + Formatting.WHITE + MINECRAFT.getCurrentFps();
+        } else {
+            fpsText = null;
+        }
+
+        int ping = PING_MANAGER.getPing();
+        pingText = "Ping: " + Formatting.WHITE + ping;
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -44,49 +94,33 @@ public class HUDModule extends Module {
         int screenWidth = MINECRAFT.getWindow().getScaledWidth();
         int screenHeight = MINECRAFT.getWindow().getScaledHeight();
 
-        if (watermark.get()) {
-            String watermark = NAME + " " + VERSION;
-            int x = PADDING;
-            int y = PADDING;
-            event.getDrawContext().drawText(MINECRAFT.textRenderer, watermark, x, y, colorInt, false);
+        int yOffset = PADDING;
+
+        if (watermarkText != null) {
+            event.getDrawContext().drawText(MINECRAFT.textRenderer, watermarkText, PADDING, yOffset, colorInt, false);
+            yOffset += MINECRAFT.textRenderer.fontHeight + 2;
         }
 
-            int ping = PING_MANAGER.getPing();
-            //boolean unstable = PING_MANAGER.isConnectionUnstable();
+        if (facingText != null) {
+            event.getDrawContext().drawText(MINECRAFT.textRenderer, facingText, PADDING, screenHeight - MINECRAFT.textRenderer.fontHeight * 2 - PADDING - 2, colorInt, false);
+        }
 
-            String pingText = "Ping: " + Formatting.WHITE + ping;
+        if (coordsText != null) {
+            event.getDrawContext().drawText(MINECRAFT.textRenderer, coordsText, PADDING, screenHeight - MINECRAFT.textRenderer.fontHeight - PADDING, colorInt, false);
+        }
 
+        if (pingText != null) {
             int pingX = screenWidth - MINECRAFT.textRenderer.getWidth(pingText) - PADDING;
             int pingY = screenHeight - MINECRAFT.textRenderer.fontHeight * 2 - PADDING - 2;
-
             event.getDrawContext().drawText(MINECRAFT.textRenderer, pingText, pingX, pingY, colorInt, false);
-
-        if (FPS.get()) {
-            int fps = MINECRAFT.getCurrentFps();
-            String f = "FPS: " + Formatting.WHITE + fps;
-            int x = screenWidth - MINECRAFT.textRenderer.getWidth(f) - PADDING;
-            int y = screenHeight - MINECRAFT.textRenderer.fontHeight - PADDING;
-            event.getDrawContext().drawText(MINECRAFT.textRenderer, f, x, y, colorInt, false);
         }
 
-        if (cords.get()) {
-            String coords = "XYZ " + Formatting.WHITE +
-                    formatCoord(MINECRAFT.player.getX()) + Formatting.GRAY + ", " + Formatting.WHITE +
-                    formatCoord(MINECRAFT.player.getY()) + Formatting.GRAY + ", " + Formatting.WHITE +
-                    formatCoord(MINECRAFT.player.getZ());
-            int x = PADDING;
-            int y = screenHeight - MINECRAFT.textRenderer.fontHeight - PADDING;
-            event.getDrawContext().drawText(MINECRAFT.textRenderer, coords, x, y, colorInt, false);
-        }
-
-        if (facing.get()) {
-            String facing = getFacing(MINECRAFT);
-            int x = PADDING;
-            int y = screenHeight - MINECRAFT.textRenderer.fontHeight * 2 - PADDING - 2;
-            event.getDrawContext().drawText(MINECRAFT.textRenderer, facing, x, y, colorInt, false);
+        if (fpsText != null) {
+            int fpsX = screenWidth - MINECRAFT.textRenderer.getWidth(fpsText) - PADDING;
+            int fpsY = screenHeight - MINECRAFT.textRenderer.fontHeight - PADDING;
+            event.getDrawContext().drawText(MINECRAFT.textRenderer, fpsText, fpsX, fpsY, colorInt, false);
         }
     }
-
 
     private String formatCoord(double coord) {
         return String.format("%.1f", coord).replace(',', '.');
