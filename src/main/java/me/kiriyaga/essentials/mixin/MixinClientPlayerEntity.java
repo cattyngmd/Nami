6394d7d2fe3c50b40f7b46b2a1819873ef0f9d7a
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-06-11 19:29:58 +0200

    nah

--- src/main/java/me/kiriyaga/essentials/mixin/MixinClientPlayerEntity.java
+++ src/main/java/me/kiriyaga/essentials/mixin/MixinClientPlayerEntity.java
@@ -5,12 +5,8 @@ import net.minecraft.client.input.Input;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
@@ -28,26 +24,10 @@ import static me.kiriyaga.essentials.Essentials.ROTATION_MANAGER;
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity {
 
-    @Shadow
-    public Input input;
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void tickHook(CallbackInfo ci) {
 
         EVENT_MANAGER.post(new UpdateEvent());
-    }
-
-    @Inject(method = "getYaw", at = @At("HEAD"), cancellable = true)
-    private void onGetYaw(CallbackInfoReturnable<Float> cir) {
-        if (ROTATION_MANAGER.isRotating()) {
-            cir.setReturnValue(ROTATION_MANAGER.getRotationYaw());
-        }
-    }
-
-    @Inject(method = "getPitch", at = @At("HEAD"), cancellable = true)
-    private void onGetPitch(CallbackInfoReturnable<Float> cir) {
-        if (ROTATION_MANAGER.isRotating()) {
-            cir.setReturnValue(ROTATION_MANAGER.getRotationPitch());
-        }
     }
 }
