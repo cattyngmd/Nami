b19dbb3815f0c2872b15bded52850d350cfbae2a
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-06-12 14:53:08 +0200

    vers

--- src/main/java/me/kiriyaga/essentials/Essentials.java
+++ src/main/java/me/kiriyaga/essentials/Essentials.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Essentials implements ClientModInitializer {
     public static String NAME = "2bEssentials";
-    public static final String VERSION = "420.8";
+    public static final String VERSION = "420.9";
 
     public static final MinecraftClient MINECRAFT = MinecraftClient.getInstance();
 
