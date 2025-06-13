69a5c962b6d9053b541cbe1c322ec6316f376885
Sawako Taiga <noisetide@proton.me>
2025-06-13 21:49:18 +1000

    Change min rotation threshold for rotation manager

--- src/main/java/me/kiriyaga/essentials/feature/module/impl/client/RotationManagerModule.java
+++ src/main/java/me/kiriyaga/essentials/feature/module/impl/client/RotationManagerModule.java
@@ -9,7 +9,7 @@ import me.kiriyaga.essentials.setting.impl.IntSetting;
 public class RotationManagerModule extends Module {
     public final DoubleSetting rotationSpeed = addSetting(new DoubleSetting("Rotation Speed", 5, 1, 30));
     public final DoubleSetting rotationEaseFactor = addSetting(new DoubleSetting("Rotation Ease Factor", 0, 0.05, 1));
-    public final DoubleSetting rotationThreshold = addSetting(new DoubleSetting("Rotation Threshold", 5, 1, 15));
+    public final DoubleSetting rotationThreshold = addSetting(new DoubleSetting("Rotation Threshold", 5, 0, 15));
     public final IntSetting ticksBeforeRelease = addSetting(new IntSetting("Rotation Hold", 40, 10, 80));
     public final IntSetting holdTicksLimit = addSetting(new IntSetting("Hold Limit", 5, 1, 50));
 
