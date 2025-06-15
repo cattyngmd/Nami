3ce988228898f935977b815f7c1b0e88be6c97c1
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-06-15 20:48:15 +0200

    fix i guess?

--- src/main/java/me/kiriyaga/essentials/mixin/MixinClientPlayerEntity.java
+++ src/main/java/me/kiriyaga/essentials/mixin/MixinClientPlayerEntity.java
@@ -1,8 +1,8 @@
 package me.kiriyaga.essentials.mixin;
 
-import me.kiriyaga.essentials.event.impl.KeyboardInputEvent;
 import me.kiriyaga.essentials.event.impl.PostTickEvent;
 import me.kiriyaga.essentials.event.impl.PreTickEvent;
+import me.kiriyaga.essentials.feature.module.impl.render.FreecamModule;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.PlayerInput;
 import org.spongepowered.asm.mixin.*;
@@ -12,13 +12,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import static me.kiriyaga.essentials.Essentials.EVENT_MANAGER;
+import static me.kiriyaga.essentials.Essentials.MODULE_MANAGER;
 
 @Mixin(ClientPlayerEntity.class)
 public class MixinClientPlayerEntity {
 
-    @Shadow
-    @Mutable
-    private PlayerInput lastPlayerInput;
+    @Shadow public PlayerInput input;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void tickHookPre(CallbackInfo ci) {
@@ -32,32 +31,11 @@ public class MixinClientPlayerEntity {
         EVENT_MANAGER.post(new PostTickEvent());
     }
 
-        @Inject(method = "tick", at = @At("HEAD"))
-        private void onTick (CallbackInfo ci){
-            PlayerInput oldInput = lastPlayerInput;
-
-            KeyboardInputEvent event = new KeyboardInputEvent(
-                    oldInput.forward(),
-                    oldInput.backward(),
-                    oldInput.left(),
-                    oldInput.right(),
-                    oldInput.jump(),
-                    oldInput.sneak(),
-                    oldInput.sprint(),
-                    lastPlayerInput
-            );
-
-
-            EVENT_MANAGER.post(event);
-
-            lastPlayerInput = new PlayerInput(
-                    event.isForward(),
-                    event.isBackward(),
-                    event.isLeft(),
-                    event.isRight(),
-                    event.isJump(),
-                    event.isSneak(),
-                    event.isSprint()
-            );
+    @Inject(method = "tickMovementInput", at = @At("HEAD"))
+    private void onTickMovementInput(CallbackInfo ci) {
+        FreecamModule module = MODULE_MANAGER.getModule(FreecamModule.class);
+        if (module.isEnabled()) {
+            this.input = new PlayerInput(false, false, false, false, false, false, false);
         }
+    }
 }
