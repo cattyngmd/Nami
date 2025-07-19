54ded7f9490a5868a4f5374462c2695480d3b411
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-07-19 15:34:56 +0200

    unhardcode

--- src/main/java/me/kiriyaga/nami/core/command/CommandRegistry.java
+++ src/main/java/me/kiriyaga/nami/core/command/CommandRegistry.java
@@ -1,22 +1,15 @@
 package me.kiriyaga.nami.core.command;
 
+import me.kiriyaga.nami.util.ClasspathScanner;
 import me.kiriyaga.nami.feature.command.Command;
 import me.kiriyaga.nami.feature.command.RegisterCommand;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.util.Set;
 
 public class CommandRegistry {
 
-    private static final String COMMAND_PACKAGE = "me.kiriyaga.nami.feature.command.impl";
-
     public static void registerAnnotatedCommands(CommandStorage storage) {
-        Set<Class<? extends Command>> classes = findAnnotatedCommandClasses();
+        Set<Class<? extends Command>> classes = ClasspathScanner.findAnnotated(Command.class, RegisterCommand.class);
 
         for (Class<? extends Command> clazz : classes) {
             try {
@@ -27,76 +20,5 @@ public class CommandRegistry {
                 e.printStackTrace();
             }
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static Set<Class<? extends Command>> findAnnotatedCommandClasses() {
-        Set<Class<? extends Command>> result = new HashSet<>();
-
-        try {
-            String path = COMMAND_PACKAGE.replace('.', '/');
-            Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources(path);
-
-            while (resources.hasMoreElements()) {
-                URL url = resources.nextElement();
-                String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
-
-                if (filePath.startsWith("file:") && filePath.contains("!")) {
-                    String[] split = filePath.split("!");
-                    try (JarFile jar = new JarFile(split[0].substring("file:".length()))) {
-                        result.addAll(scanJar(jar));
-                    }
-                } else {
-                    File dir = new File(filePath);
-                    if (dir.exists() && dir.isDirectory()) {
-                        result.addAll(scanDirectory(dir, COMMAND_PACKAGE));
-                    }
-                }
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return result;
-    }
-
-    private static Set<Class<? extends Command>> scanDirectory(File dir, String packageName) throws ClassNotFoundException {
-        Set<Class<? extends Command>> result = new HashSet<>();
-
-        for (File file : Objects.requireNonNull(dir.listFiles())) {
-            if (file.isDirectory()) {
-                result.addAll(scanDirectory(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                String className = packageName + '.' + file.getName().replace(".class", "");
-                Class<?> cls = Class.forName(className);
-
-                if (Command.class.isAssignableFrom(cls) && cls.isAnnotationPresent(RegisterCommand.class)) {
-                    result.add((Class<? extends Command>) cls);
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private static Set<Class<? extends Command>> scanJar(JarFile jar) throws ClassNotFoundException {
-        Set<Class<? extends Command>> result = new HashSet<>();
-
-        Enumeration<JarEntry> entries = jar.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String name = entry.getName();
-
-            if (name.endsWith(".class") && name.startsWith(COMMAND_PACKAGE.replace('.', '/'))) {
-                String className = name.replace('/', '.').replace(".class", "");
-                Class<?> cls = Class.forName(className);
-
-                if (Command.class.isAssignableFrom(cls) && cls.isAnnotationPresent(RegisterCommand.class)) {
-                    result.add((Class<? extends Command>) cls);
-                }
-            }
-        }
-
-        return result;
     }
 }
---
+++ src/main/java/me/kiriyaga/nami/util/ClasspathScanner.java
@@ -0,0 +1,84 @@
+package me.kiriyaga.nami.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class ClasspathScanner {
+
+    @SuppressWarnings("unchecked")
+    public static <T> Set<Class<? extends T>> findAnnotated(Class<T> baseClass, Class<?> annotation) {
+        Set<Class<? extends T>> result = new HashSet<>();
+
+        try {
+            Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources("");
+
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
+
+                if (filePath.startsWith("file:") && filePath.contains("!")) {
+                    String[] split = filePath.split("!");
+                    try (JarFile jar = new JarFile(split[0].substring("file:".length()))) {
+                        result.addAll(scanJar(jar, baseClass, annotation));
+                    }
+                } else {
+                    File dir = new File(filePath);
+                    if (dir.exists() && dir.isDirectory()) {
+                        result.addAll(scanDirectory(dir, "", baseClass, annotation));
+                    }
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private static <T> Set<Class<? extends T>> scanDirectory(File dir, String pkg, Class<T> base, Class<?> annotation) throws ClassNotFoundException {
+        Set<Class<? extends T>> result = new HashSet<>();
+
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            String fullPkg = pkg.isEmpty() ? "" : pkg + ".";
+            if (file.isDirectory()) {
+                result.addAll(scanDirectory(file, fullPkg + file.getName(), base, annotation));
+            } else if (file.getName().endsWith(".class")) {
+                String className = fullPkg + file.getName().replace(".class", "");
+                Class<?> cls = Class.forName(className);
+
+                if (base.isAssignableFrom(cls) && cls.isAnnotationPresent((Class<? extends Annotation>) annotation)) {
+                    result.add((Class<? extends T>) cls);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static <T> Set<Class<? extends T>> scanJar(JarFile jar, Class<T> base, Class<?> annotation) throws ClassNotFoundException {
+        Set<Class<? extends T>> result = new HashSet<>();
+        Enumeration<JarEntry> entries = jar.entries();
+
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            String name = entry.getName();
+
+            if (name.endsWith(".class")) {
+                String className = name.replace('/', '.').replace(".class", "");
+                Class<?> cls = Class.forName(className, false, Thread.currentThread().getContextClassLoader());
+
+                if (base.isAssignableFrom(cls) && cls.isAnnotationPresent((Class<? extends Annotation>) annotation)) {
+                    result.add((Class<? extends T>) cls);
+                }
+            }
+        }
+
+        return result;
+    }
+}
