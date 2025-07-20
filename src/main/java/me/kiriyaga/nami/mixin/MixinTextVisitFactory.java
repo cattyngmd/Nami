170bda43550b915e383ee9dede263a97bae65149
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-07-20 16:52:48 +0200

    hotfix

--- src/main/java/me/kiriyaga/nami/mixin/MixinTextVisitFactory.java
+++ src/main/java/me/kiriyaga/nami/mixin/MixinTextVisitFactory.java
@@ -14,6 +14,9 @@ public class MixinTextVisitFactory implements IMinecraft {
 
     @ModifyVariable(method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private static String replaceText(String value) {
+        if (MODULE_MANAGER.getStorage() == null || MODULE_MANAGER.getStorage().getByClass(NameProtectModule.class) == null)
+            return value;
+
         if (MODULE_MANAGER.getStorage().getByClass(NameProtectModule.class).isEnabled()) return value.replaceAll(mc.getSession().getUsername(), "NamiClient"); // TODO unhardcode that. the day im gonna write runtime lists
         return value;
     }
