3154a1c67e4b719990e9f1fc5d79fd0f0155196b
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-07-21 19:20:55 +0200

    visual range module

---
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/world/VisualRangeModule.java
@@ -0,0 +1,47 @@
+package me.kiriyaga.nami.feature.module.impl.world;
+
+import me.kiriyaga.nami.event.EventPriority;
+import me.kiriyaga.nami.event.SubscribeEvent;
+import me.kiriyaga.nami.event.impl.PacketReceiveEvent;
+import me.kiriyaga.nami.feature.module.Module;
+import me.kiriyaga.nami.feature.module.ModuleCategory;
+import me.kiriyaga.nami.feature.module.RegisterModule;
+import me.kiriyaga.nami.setting.impl.BoolSetting;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.sound.SoundEvents;
+
+import static me.kiriyaga.nami.Nami.*;
+
+@RegisterModule(category = "world")
+public class VisualRangeModule extends Module {
+
+    private final BoolSetting friends = addSetting(new BoolSetting("friends", false));
+    private final BoolSetting sound = addSetting(new BoolSetting("sound", false));
+
+    public VisualRangeModule() {
+        super("visual range", "Notifies you when players enter render distance.", ModuleCategory.of("world"), "visualrange", "мшыгфдкфтпу");
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onPacketReceive(PacketReceiveEvent event) {
+        if (MC.player == null || MC.world == null) return;
+
+        if (event.getPacket() instanceof EntitySpawnS2CPacket spawnPacket) {
+            int entityId = spawnPacket.getEntityId();
+
+            MC.execute(() -> {
+                var entity = MC.world.getEntityById(entityId);
+                if (entity instanceof PlayerEntity player) {
+                    if (player == MC.player) return;
+                    if (FRIEND_MANAGER.isFriend(player.getName().getString()) && friends.get()) return;
+
+                    CHAT_MANAGER.sendPersistent(player.getUuidAsString(), "§7" + player.getName().getString() + "§f has entered visual range.");
+
+                    if (sound.get())
+                        MC.player.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.0f);
+                }
+            });
+        }
+    }
+}
