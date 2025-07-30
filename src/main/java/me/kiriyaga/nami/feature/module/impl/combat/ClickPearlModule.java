9a5f0355dda40834d7a356bcb0b762bdb976e08f
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-07-30 11:32:53 +0200

    feat: glide firework

--- src/main/java/me/kiriyaga/nami/feature/module/impl/combat/ClickPearlModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/combat/ClickPearlModule.java
@@ -4,6 +4,7 @@ import me.kiriyaga.nami.feature.module.Module;
 import me.kiriyaga.nami.feature.module.ModuleCategory;
 import me.kiriyaga.nami.feature.module.RegisterModule;
 import me.kiriyaga.nami.setting.impl.BoolSetting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
@@ -13,6 +14,8 @@ import static me.kiriyaga.nami.Nami.MC;
 
 @RegisterModule
 public class ClickPearlModule extends Module {
+
+    public final BoolSetting glideFirework = addSetting(new BoolSetting("glide firework", true));
 
     public ClickPearlModule() {
         super("click pearl", "Throws a pearl on activation.", ModuleCategory.of("combat"), "clickpearl", "сдшслашкуцщкл");
@@ -24,7 +27,18 @@ public class ClickPearlModule extends Module {
         if (MC.world == null || MC.player == null)
             return;
 
-        int i = getPearlSlot();
+        if (MC.player.isGliding() && glideFirework.get()){
+            int i = getSlot(Items.FIREWORK_ROCKET);
+            if (i == -1)
+                return;
+
+            INVENTORY_MANAGER.getSlotHandler().attemptSwitch(i);
+            MC.interactionManager.interactItem(MC.player, Hand.MAIN_HAND);
+            INVENTORY_MANAGER.getSyncHandler().swapSync();
+            return;
+        }
+
+        int i = getSlot(Items.ENDER_PEARL);
         if (i == -1 || MC.player.getItemCooldownManager().isCoolingDown(Items.ENDER_PEARL.getDefaultStack()))
             return;
 
@@ -34,10 +48,10 @@ public class ClickPearlModule extends Module {
         INVENTORY_MANAGER.getSyncHandler().swapSync();
     }
 
-    private int getPearlSlot() {
+    private int getSlot(Item item) {
         for (int i = 0; i < 9; i++) {
             ItemStack stack = MC.player.getInventory().getStack(i);
-            if (stack.getItem() == Items.ENDER_PEARL) return i;
+            if (stack.getItem() == item) return i;
         }
         return -1;
     }
