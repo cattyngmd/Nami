dc25b91ab5e3d44d9d6e2319b4777b2d99a4a999
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-08-11 12:35:56 +0200

    feat: hudeditor screen background

--- src/main/java/me/kiriyaga/nami/feature/module/impl/client/HudEditorModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/client/HudEditorModule.java
@@ -4,13 +4,19 @@ import me.kiriyaga.nami.feature.gui.screen.HudEditorScreen;
 import me.kiriyaga.nami.feature.module.Module;
 import me.kiriyaga.nami.feature.module.ModuleCategory;
 import me.kiriyaga.nami.feature.module.RegisterModule;
+import me.kiriyaga.nami.setting.impl.BoolSetting;
+import me.kiriyaga.nami.setting.impl.IntSetting;
 
 import static me.kiriyaga.nami.Nami.MC;
 
 @RegisterModule
 public class HudEditorModule extends Module {
+    public final BoolSetting background = addSetting(new BoolSetting("background", false));
+    public final IntSetting backgroundAlpha = addSetting(new IntSetting("background alpha", 200, 0, 255));
+
     public HudEditorModule() {
         super("hud editor", "Opens HUD editor screen.", ModuleCategory.of("client"), "hudeditor", "hudedit", "he", "худ", "худе");
+        backgroundAlpha.setShowCondition(background::get);
     }
 
     @Override
