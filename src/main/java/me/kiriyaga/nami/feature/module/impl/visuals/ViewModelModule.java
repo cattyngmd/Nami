e68270214f66b0d5c1653e6b77e9be1471bf2991
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-08-22 15:17:00 +0200

    refactor: viewmodule

--- src/main/java/me/kiriyaga/nami/feature/module/impl/visuals/ViewModelModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/visuals/ViewModelModule.java
@@ -2,7 +2,6 @@ package me.kiriyaga.nami.feature.module.impl.visuals;
 
 import me.kiriyaga.nami.event.EventPriority;
 import me.kiriyaga.nami.event.SubscribeEvent;
-import me.kiriyaga.nami.event.impl.HeldItemRendererEvent;
 import me.kiriyaga.nami.feature.module.Module;
 import me.kiriyaga.nami.feature.module.ModuleCategory;
 import me.kiriyaga.nami.feature.module.RegisterModule;
@@ -11,46 +10,19 @@ import me.kiriyaga.nami.setting.impl.DoubleSetting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.RotationAxis;
 
-import static me.kiriyaga.nami.Nami.MC;
-
 @RegisterModule
 public class ViewModelModule extends Module {
 
-    public final DoubleSetting scale = addSetting(new DoubleSetting("scale", 1.0, 0.1, 1.6));
-    public final DoubleSetting posX = addSetting(new DoubleSetting("posX", 0.0, -0.6, 0.6));
-    public final DoubleSetting posY = addSetting(new DoubleSetting("posY", 0.0, -0.6, 0.6));
-    public final DoubleSetting posZ = addSetting(new DoubleSetting("posZ", 0.0, -0.6, 0.6));
+    public final BoolSetting eating = addSetting(new BoolSetting("eating", false));
+    public final DoubleSetting scale = addSetting(new DoubleSetting("scale", 1.0, 0.1, 2));
+    public final DoubleSetting posX = addSetting(new DoubleSetting("posX", 0.0, -3, 3));
+    public final DoubleSetting posY = addSetting(new DoubleSetting("posY", 0.0, -3, 3));
+    public final DoubleSetting posZ = addSetting(new DoubleSetting("posZ", 0.0, -3, 3));
     public final DoubleSetting rotX = addSetting(new DoubleSetting("rotX", 0.0, -180.0, 180.0));
     public final DoubleSetting rotY = addSetting(new DoubleSetting("rotY", 0.0, -180.0, 180.0));
     public final DoubleSetting rotZ = addSetting(new DoubleSetting("rotZ", 0.0, -180.0, 180.0));
 
     public ViewModelModule() {
         super("viewmodel", "Modifies hand position, scale, and rotation.", ModuleCategory.of("visuals"), "vm", "handpos");
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    private void onHeldItemRender(HeldItemRendererEvent ev) {
-        boolean isOffhand = ev.getHand() == Hand.OFF_HAND;
-        float mirror = isOffhand ? -1.0f : 1.0f;
-
-        ev.getMatrix().translate(
-                posX.get() * mirror,
-                posY.get(),
-                posZ.get()
-        );
-
-        float rx = rotX.get().floatValue();
-        float ry = rotY.get().floatValue() * mirror;
-        float rz = rotZ.get().floatValue();
-
-        ev.getMatrix().multiply(RotationAxis.POSITIVE_X.rotationDegrees(rx));
-        ev.getMatrix().multiply(RotationAxis.POSITIVE_Y.rotationDegrees(ry));
-        ev.getMatrix().multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rz));
-
-        ev.getMatrix().scale(
-                 (scale.get().floatValue()),
-                 scale.get().floatValue(),
-                scale.get().floatValue()
-        );
     }
 }
--- src/main/java/me/kiriyaga/nami/mixin/MixinHeldItemRenderer.java
+++ src/main/java/me/kiriyaga/nami/mixin/MixinHeldItemRenderer.java
@@ -1,18 +1,23 @@
 package me.kiriyaga.nami.mixin;
 
-import me.kiriyaga.nami.event.impl.HeldItemRendererEvent;
 import me.kiriyaga.nami.feature.module.impl.visuals.OldAnimationsModule;
+import me.kiriyaga.nami.feature.module.impl.visuals.ViewModelModule;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static me.kiriyaga.nami.Nami.*;
@@ -49,10 +54,35 @@ public abstract class MixinHeldItemRenderer {
     }
 
     @Inject(method = "renderFirstPersonItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemDisplayContext;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", shift = At.Shift.BEFORE))
-    private void onRenderItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-        HeldItemRendererEvent ev = new HeldItemRendererEvent(hand, matrices);
+    private void renderFirstPersonItem(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        ViewModelModule vm = MODULE_MANAGER.getStorage().getByClass(ViewModelModule.class);
+        if (vm != null && vm.isEnabled()) {
+            boolean isOffhand = hand == Hand.OFF_HAND;
+            float mirror = isOffhand ? -1.0f : 1.0f;
 
-        EVENT_MANAGER.post(ev);
+            matrices.translate(
+                    vm.posX.get() * mirror,
+                    vm.posY.get(),
+                    vm.posZ.get()
+            );
+
+            float rx = vm.rotX.get().floatValue();
+            float ry = vm.rotY.get().floatValue() * mirror;
+            float rz = vm.rotZ.get().floatValue();
+
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(rx));
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(ry));
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rz));
+
+            float s = vm.scale.get().floatValue();
+            matrices.scale(s, s, s);
+        }
     }
 
+    @Inject(method = "applyEatOrDrinkTransformation", at = @At("HEAD"), cancellable = true)
+    private void applyEatOrDrinkTransformation(MatrixStack matrixStack, float tickDelta, Arm arm, ItemStack stack, PlayerEntity player, CallbackInfo ci) {
+        ViewModelModule vm = MODULE_MANAGER.getStorage().getByClass(ViewModelModule.class);
+        if (vm != null && vm.isEnabled() && !vm.eating.get())
+            ci.cancel();
+    }
 }
