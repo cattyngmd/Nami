4b2211ff19904a0922d03fbe592d0b1af7518d9c
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-08-24 12:57:44 +0200

    WIP glide

--- src/main/java/me/kiriyaga/nami/feature/module/impl/movement/ElytraFlyModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/movement/ElytraFlyModule.java
@@ -16,8 +16,11 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
 
 import static me.kiriyaga.nami.Nami.*;
@@ -26,13 +29,22 @@ import static me.kiriyaga.nami.Nami.*;
 public class ElytraFlyModule extends Module {
 
     public enum FlyMode {
-        BOUNCE, CONTROL
+        BOUNCE, CONTROL, GLIDE
     }
 
-
     public final EnumSetting<FlyMode> mode = addSetting(new EnumSetting<>("mode", FlyMode.BOUNCE));
+
+    // GLIDE
+    private final IntSetting minSpeed = addSetting(new IntSetting("min speed", 8, 1, 30));
+    private final IntSetting climbPitch = addSetting(new IntSetting("climb pitch", 27, 0, 89));
+    private final IntSetting divePitch = addSetting(new IntSetting("dive pitch", 38, 0, 89));
+    private final IntSetting cruisePitch = addSetting(new IntSetting("cruise pitch", 8, 0, 30));
+
+    // CONTROL
     //private final BoolSetting midAirFreeze = addSetting(new BoolSetting("mid air freeze", false));
     private final BoolSetting lockPitch = addSetting(new BoolSetting("lock pitch", true));
+
+    // BOOST
     private final BoolSetting boost = addSetting(new BoolSetting("boost", false));
     private final BoolSetting newBoost = addSetting(new BoolSetting("new boost", false));
     private final BoolSetting pitch = addSetting(new BoolSetting("pitch", true));
@@ -43,6 +55,7 @@ public class ElytraFlyModule extends Module {
     private double[] speedSamples = new double[]{25};
     private int speedSampleIndex = 0;
     private boolean speedBufferFilled = false;
+    private double baseY = 0;
 
     private double lastX = 0;
     private double lastZ = 0;
@@ -55,13 +68,23 @@ public class ElytraFlyModule extends Module {
         pitchDegree.setShowCondition(() -> mode.get() == FlyMode.BOUNCE && pitch.get());
         lockPitch.setShowCondition(() -> mode.get() == FlyMode.CONTROL);
         //midAirFreeze.setShowCondition(() -> mode.get() == FlyMode.CONTROL);
+        minSpeed.setShowCondition(() -> mode.get() == FlyMode.GLIDE);
+        climbPitch.setShowCondition(() -> mode.get() == FlyMode.GLIDE);
+        divePitch.setShowCondition(() -> mode.get() == FlyMode.GLIDE);
+        cruisePitch.setShowCondition(() -> mode.get() == FlyMode.GLIDE);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        if (MC.player != null)
+            baseY = MC.player.getY();
     }
 
     @Override
     public void onDisable() {
-
-        if (mode.get() == FlyMode.BOUNCE)
-            setJumpHeld(false);
+        if (mode.get() == FlyMode.BOUNCE) setJumpHeld(false);
+        baseY = 0;
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -148,8 +171,33 @@ public class ElytraFlyModule extends Module {
 
                 setJumpHeld(true);
             }
+        } else if (mode.get() == FlyMode.GLIDE) {
+
+            float currentPitch = ROTATION_MANAGER.getStateHandler().getRotationPitch();
+            float targetPitch;
+
+            if (speed < minSpeed.get()) {
+                useItemAnywhere(Items.FIREWORK_ROCKET);
+                targetPitch = -climbPitch.get().floatValue();
+            } else {
+                if (MC.player.getY() > baseY + 2) {
+                    targetPitch = divePitch.get().floatValue();
+                } else {
+                    targetPitch = cruisePitch.get().floatValue() * (Math.sin(System.currentTimeMillis() * 0.005) > 0 ? 1 : -1);
+                }
+            }
+
+            float maxDelta = 10f;
+            float delta = targetPitch - currentPitch;
+            if (delta > maxDelta) delta = maxDelta;
+            if (delta < -maxDelta) delta = -maxDelta;
+            float smoothPitch = currentPitch + delta;
+
+            ROTATION_MANAGER.getRequestHandler().submit(
+                    new RotationRequest(this.getName(), rotationPriority.get(), MC.player.getYaw(), smoothPitch)
+            );
         }
-}
+    }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     private void onPreTick2(PreTickEvent event) {
@@ -221,5 +269,50 @@ public class ElytraFlyModule extends Module {
         Vec3d worldDir = new Vec3d(wx, 0.0, wz);
         if (worldDir.lengthSquared() == 0.0) return null;
         return worldDir.normalize();
+    }
+
+    private boolean useItemAnywhere(Item item) {
+        int hotbarSlot = getSlotInHotbar(item);
+
+        if (hotbarSlot != -1) {
+            int prevSlot = MC.player.getInventory().getSelectedSlot();
+            INVENTORY_MANAGER.getSlotHandler().attemptSwitch(hotbarSlot);
+            MC.interactionManager.interactItem(MC.player, Hand.MAIN_HAND);
+            INVENTORY_MANAGER.getSlotHandler().attemptSwitch(prevSlot);
+            return true;
+        }
+
+        int invSlot = getSlotInInventory(item);
+        if (invSlot != -1) {
+            int selectedHotbarIndex = MC.player.getInventory().getSelectedSlot();
+            int containerInvSlot = convertSlot(invSlot);
+
+            INVENTORY_MANAGER.getClickHandler().swapSlot(containerInvSlot, selectedHotbarIndex);
+            MC.interactionManager.interactItem(MC.player, Hand.MAIN_HAND);
+            INVENTORY_MANAGER.getClickHandler().swapSlot(containerInvSlot, selectedHotbarIndex);
+            return true;
+        }
+
+        return false;
+    }
+
+    private int getSlotInHotbar(Item item) {
+        for (int i = 0; i < 9; i++) {
+            ItemStack stack = MC.player.getInventory().getStack(i);
+            if (stack.getItem() == item) return i;
+        }
+        return -1;
+    }
+
+    private int getSlotInInventory(Item item) {
+        for (int i = 9; i < 36; i++) {
+            ItemStack stack = MC.player.getInventory().getStack(i);
+            if (stack.getItem() == item) return i;
+        }
+        return -1;
+    }
+
+    private int convertSlot(int slot) {
+        return slot < 9 ? slot + 36 : slot;
     }
 }
