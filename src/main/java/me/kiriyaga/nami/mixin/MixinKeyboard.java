5d8dff8180391b312e21b39c10b13601fb3a660f
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-08-29 11:54:24 +0200

    fix: focused keyboard

--- src/main/java/me/kiriyaga/nami/mixin/MixinKeyboard.java
+++ src/main/java/me/kiriyaga/nami/mixin/MixinKeyboard.java
@@ -1,9 +1,15 @@
 package me.kiriyaga.nami.mixin;
 
 import me.kiriyaga.nami.event.impl.KeyInputEvent;
+import me.kiriyaga.nami.feature.gui.screen.ClickGuiScreen;
 import me.kiriyaga.nami.feature.module.impl.client.ClickGuiModule;
 import me.kiriyaga.nami.setting.impl.KeyBindSetting;
 import net.minecraft.client.Keyboard;
+import net.minecraft.client.gui.EditBox;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -41,9 +47,17 @@ public abstract class MixinKeyboard {
         if (bind == null) return;
 
         if (bind.get() == key) {
-            if (MC.world == null) {
-                clickGui.toggle();
+            Screen screen = MC.currentScreen;
+
+            if (screen instanceof TitleScreen
+                    || screen instanceof SelectWorldScreen
+                    || screen instanceof MultiplayerScreen) {
+
+                if (!(screen.getFocused() instanceof EditBox)) {
+                    clickGui.toggle();
+                }
             }
         }
     }
+
 }
