65030c9d75f1a283675e636c6a665e85797541d4
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-09-15 21:29:02 +0200

    feat: auto tool

---
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/world/AutoToolModule.java
@@ -0,0 +1,62 @@
+package me.kiriyaga.nami.feature.module.impl.world;
+
+import me.kiriyaga.nami.core.executable.model.ExecutableEventType;
+import me.kiriyaga.nami.event.EventPriority;
+import me.kiriyaga.nami.event.SubscribeEvent;
+import me.kiriyaga.nami.event.impl.BreakBlockEvent;
+import me.kiriyaga.nami.event.impl.StartBreakingBlockEvent;
+import me.kiriyaga.nami.feature.module.Module;
+import me.kiriyaga.nami.feature.module.ModuleCategory;
+import me.kiriyaga.nami.feature.module.RegisterModule;
+import me.kiriyaga.nami.util.EnchantmentUtils;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
+
+import static me.kiriyaga.nami.Nami.*;
+
+@RegisterModule
+public class AutoToolModule extends Module {
+
+    public AutoToolModule() {
+        super("auto tool", "Auto selects the currently best mining tool from your hotbar.", ModuleCategory.of("world"), "autotool");
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    private void onBlockAttack(StartBreakingBlockEvent event) {
+        if (MC.player == null || MC.world == null || MC.player.getGameMode() != GameMode.SURVIVAL) {
+            return;
+        }
+
+        EXECUTABLE_MANAGER.getRequestHandler().submit(() -> { // we are not on main thread!
+            BlockPos targetPos = event.blockPos;
+            BlockState targetState = MC.world.getBlockState(targetPos);
+
+            int bestSlot = -1;
+            float bestSpeed = 1.0f;
+
+            for (int slot = 0; slot < 9; slot++) {
+                ItemStack stack = MC.player.getInventory().getStack(slot);
+                if (stack.isEmpty()) {
+                    continue;
+                }
+
+                float efficiencyLevel = EnchantmentUtils.getEnchantmentLevel(stack, Enchantments.EFFICIENCY);
+                float miningSpeed = stack.getMiningSpeedMultiplier(targetState);
+                float totalSpeed = efficiencyLevel + miningSpeed;
+
+                if (totalSpeed > bestSpeed) {
+                    bestSpeed = totalSpeed;
+                    bestSlot = slot;
+                }
+            }
+
+            if (bestSlot != -1)
+                INVENTORY_MANAGER.getSlotHandler().attemptSwitch(bestSlot);
+            }, 0, ExecutableEventType.PRE_TICK);
+    }
+}
