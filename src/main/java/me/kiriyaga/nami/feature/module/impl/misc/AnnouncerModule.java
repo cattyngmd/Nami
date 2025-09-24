f332ba72e40e469a994880f0f3ae8941e6ecab0d
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-09-24 17:12:56 +0200

    refactor: sound

--- src/main/java/me/kiriyaga/nami/feature/module/impl/misc/AnnouncerModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/misc/AnnouncerModule.java
@@ -17,25 +17,21 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 
 import static me.kiriyaga.nami.Nami.*;
-import static me.kiriyaga.nami.feature.module.impl.world.VisualRangeModule.VisualRangeMode.BELL;
-import static me.kiriyaga.nami.feature.module.impl.world.VisualRangeModule.VisualRangeMode.EXP;
 
 @RegisterModule
 public class AnnouncerModule extends Module {
     public enum VisualRangeMode {
-        BELL, EXP
+        NONE, BELL, EXP
     }
 
     public final BoolSetting everyone = addSetting(new BoolSetting("Everyone", false));
     public final BoolSetting friends = addSetting(new BoolSetting("Friends", true));
     public final BoolSetting joinAnnounce = addSetting(new BoolSetting("JoinAnnounce", false));
     public final BoolSetting visualRange = addSetting(new BoolSetting("VisualRange", false));
-    private final BoolSetting sound = addSetting(new BoolSetting("Sound", false));
-    private final EnumSetting soundMode = addSetting(new EnumSetting("Sound", EXP));
+    private final EnumSetting<VisualRangeMode> soundMode = addSetting(new EnumSetting<>("Sound", VisualRangeMode.NONE));
 
     public AnnouncerModule() {
-        super("Announcer", "Announces in chat when a certain action happened.", ModuleCategory.of("Misc"), "joinannounce", "joins", "announce");
-    sound.setShowCondition(visualRange::get);
+        super("Announcer", "Announces in chat when a certain action happened.", ModuleCategory.of("Misc"), "joinannounce", "joins", "announce", "visualrange");
     soundMode.setShowCondition(visualRange::get);
     }
 
@@ -96,12 +92,10 @@ public class AnnouncerModule extends Module {
             CHAT_MANAGER.sendPersistent(player.getUuidAsString(),
                     CAT_FORMAT.format("{g}" + player.getName().getString() + " {reset}has entered visual range."));
 
-            if (sound.get()) {
-                switch (soundMode.get()) {
-                    case BELL -> MC.player.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.0f);
-                    case EXP -> MC.player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
-                    default -> { }
-                }
+            switch (soundMode.get()) {
+                case BELL -> MC.player.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.0f);
+                case EXP -> MC.player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+                default -> {}
             }
         }
     }
