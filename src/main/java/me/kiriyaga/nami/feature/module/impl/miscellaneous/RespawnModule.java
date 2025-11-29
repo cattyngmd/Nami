369ecd0d64e05bd59a7d32a7f24eb8e52fef1221
Kiriyaga W <kiriyaga7615@github.com>
2025-11-29 17:11:38 +0100

    fix: log cords

--- src/main/java/me/kiriyaga/nami/feature/module/impl/miscellaneous/RespawnModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/miscellaneous/RespawnModule.java
@@ -26,31 +26,35 @@ public class RespawnModule extends Module {
         super("Respawn", "Death screen tweaks.", ModuleCategory.of("Miscellaneous"), "autorespawn");
     }
 
-    private Vec3d last;
+    boolean b = false;
 
+    @Override
+    public void onEnable() {
+        b = false;
+    }
     @SubscribeEvent
     public void onTick(PreTickEvent ev) {
-        if (MC.player != null) last = MC.player.getPos();
+        if (MC.player == null || !MC.player.isDead() || b)
+            return;
+
+        b = true;
+        if (sendCords.get()) {
+                String coords = String.format("X: %d Y: %d Z: %d", Math.round(MC.player.getPos().x), Math.round(MC.player.getPos().y), Math.round(MC.player.getPos().z));
+                Text reason = CAT_FORMAT.format("Death coordinates: {g}" + coords+"{reset}.");
+                LOG.addEntry(this.name+": "+ reason.getString());
+        }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onOpenScreen(OpenScreenEvent event) {
         if (!(event.getScreen() instanceof DeathScreen)) return;
-        if (MC == null || last == null) return;
-
-        if (sendCords.get()) {
-            EXECUTABLE_MANAGER.getRequestHandler().submit(() ->{
-                String coords = String.format("X: %d Y: %d Z: %d", Math.round(last.x), Math.round(last.y), Math.round(last.z));
-                Text reason = CAT_FORMAT.format("Death coordinates: {g}" + coords+"{reset}.");
-                LOG.addEntry(this.name+": "+ reason.getString());
-                CHAT_MANAGER.sendPersistent(RespawnModule.class.getName(), "reason");
-            }, 1, ExecutableThreadType.PRE_TICK);
-        }
+        if (MC == null) return;
 
         if (autoRespawn.get()) {
 
             MC.player.requestRespawn();
             event.cancel();
         }
+        b = false;
     }
 }
