f31caa1f8c2f75be6ff24d7329fa16813dc1a92c
Kiriyaga W <kiriyaga7615@github.com>
2025-12-01 23:17:26 +0100

    feat: setbackstop to autowalk

--- src/main/java/me/kiriyaga/nami/feature/module/impl/movement/AutoWalkModule.java
+++ src/main/java/me/kiriyaga/nami/feature/module/impl/movement/AutoWalkModule.java
@@ -6,14 +6,18 @@ import me.kiriyaga.nami.event.impl.PreTickEvent;
 import me.kiriyaga.nami.feature.module.ModuleCategory;
 import me.kiriyaga.nami.feature.module.Module;
 import me.kiriyaga.nami.feature.module.RegisterModule;
+import me.kiriyaga.nami.feature.setting.impl.BoolSetting;
 import me.kiriyaga.nami.mixin.KeyBindingAccessor;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 import static me.kiriyaga.nami.Nami.MC;
+import static me.kiriyaga.nami.Nami.SERVER_MANAGER;
 
 @RegisterModule
 public class AutoWalkModule extends Module {
+
+    private final BoolSetting setbackStop = addSetting(new BoolSetting("SetbackStop", true));
 
     public AutoWalkModule() {
         super("AutoWalk", "Automatically makes you walk.", ModuleCategory.of("Movement"),"autowalk");
@@ -25,7 +29,13 @@ public class AutoWalkModule extends Module {
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onUpdateEvent(PreTickEvent event) {
+    public void onPreTick(PreTickEvent event) {
+        if (MC.player == null || MC.world == null)
+            return;
+
+        if (setbackStop.get() && SERVER_MANAGER.getSetback(5000))
+            return;
+
         setWalkHeld(true);
     }
 
