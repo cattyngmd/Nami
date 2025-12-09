4ce7c024f930afae3888a5dac711769d46bf439b
Kiriyaga W <kiriyaga7615@github.com>
2025-12-09 23:46:29 +0100

    fix: slider alpha

--- src/main/java/me/kiriyaga/nami/feature/gui/oldgui/settings/IntSettingRenderer.java
+++ src/main/java/me/kiriyaga/nami/feature/gui/oldgui/settings/IntSettingRenderer.java
@@ -20,9 +20,11 @@ public class IntSettingRenderer implements SettingRenderer<IntSetting> {
 
         boolean hovered = isHovered(mouseX, mouseY, x, y);
 
-        Color primary = getColorModule().getStyledGlobalColor(255);
+        Color primary = getColorModule().getStyledGlobalColor();
+        Color text = getColorModule().getStyledGlobalColor(255);
+
         int bgColorInt = CLICK_GUI.applyFade(toRGBA(new Color(30, 30, 30, 0)));
-        int textColorInt = CLICK_GUI.applyFade(toRGBA(primary));
+        int textColorInt = CLICK_GUI.applyFade(toRGBA(text));
 
         context.fill(x, y, x + WIDTH, y + HEIGHT, bgColorInt);
 
