65aea315e16ea54364e08b17be253bf6b66f3e43
Kiriyaga W <kiriyaga7615@github.com>
2026-02-01 17:04:44 +0100

    fix: font custom shadow translate also affects vanilla font

--- src/main/java/namidevelopment/kiriyaga/nami/api/font/FontService.java
+++ src/main/java/namidevelopment/kiriyaga/nami/api/font/FontService.java
@@ -13,6 +13,8 @@ public class FontService {
     public final FontRendererProvider rendererProvider = new FontRendererProvider(fontLoader);
     public final FontMetrics fontMetrics = new FontMetrics(rendererProvider);
 
+    private float translate = 0.50f;
+    
     public void init() {
         fontLoader.init();
     }
@@ -21,72 +23,72 @@ public class FontService {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), text, x, y, ColorUtils.darken(ColorUtils.fromRGBA(color), FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()).getRGB(), false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, color, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, color, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public void drawText(GuiGraphics context, String text, int x, int y, boolean shadow, int color) {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), text, x, y, ColorUtils.darken(ColorUtils.fromRGBA(color), FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()).getRGB(), false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, color, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, color, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public void drawText(GuiGraphics context, Component text, int x, int y, boolean shadow) {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), ColorUtils.darken(text, FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()), x, y, 0xFFFFFFFF, false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, 0xFFFFFFFF, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, 0xFFFFFFFF, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public void drawText(GuiGraphics context, String text, int x, int y, boolean shadow) {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), text, x, y, ColorUtils.darken(ColorUtils.fromRGBA(0xFFFFFFFF), FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()).getRGB(), false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, 0xFFFFFFFF, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, 0xFFFFFFFF, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public void drawText(GuiGraphics context, Component text, int x, int y, int color, boolean shadow) {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), ColorUtils.darken(text, FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()), x, y, ColorUtils.darken(ColorUtils.fromRGBA(color), 100).getRGB(), false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, color, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, color, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public void drawText(GuiGraphics context, String text, int x, int y, int color, boolean shadow) {
         if (shadow) {
             var matrices = context.pose();
             matrices.pushMatrix();
-            matrices.translate(0.5f, 0.5f);
+            matrices.translate(translate, translate);
             context.drawString(rendererProvider.getRenderer(), text, x, y, ColorUtils.darken(ColorUtils.fromRGBA(color), FEATURE_SERVICE.getStorage().getByClass(FontFeature.class).shadowDarken.get()).getRGB(), false);
             matrices.popMatrix();
         }
 
-        context.drawString(rendererProvider.getRenderer(), text, x, y, color, false);
+        context.drawString(rendererProvider.getRenderer(), text, x, y, color, rendererProvider.getRenderer().equals(MC.font));
     }
 
     public int getWidth(Component text) {
