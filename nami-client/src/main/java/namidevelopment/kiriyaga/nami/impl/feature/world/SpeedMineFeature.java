fbb1a0fbd706b626b12ad47d4e25fad24fcb4243
Kiriyaga W <kiriyaga7615@github.com>
2026-02-13 14:01:32 +0100

    fix: SpeedMine async consistency

--- nami-client/src/main/java/namidevelopment/kiriyaga/nami/impl/feature/world/SpeedMineFeature.java
+++ nami-client/src/main/java/namidevelopment/kiriyaga/nami/impl/feature/world/SpeedMineFeature.java
@@ -58,8 +58,8 @@ public class SpeedMineFeature extends Feature {
     public final BoolSetting grim = addSetting(new BoolSetting("Grim", false));
     public final BoolSetting doubleMine = addSetting(new BoolSetting("DoubleMine", false));
     public final BoolSetting instant = addSetting(new BoolSetting("Instant", true));
+    public final BoolSetting asyncRemine = addSetting(new BoolSetting("AsyncRemine", true));
     public final BoolSetting swing = addSetting(new BoolSetting("Swing", true));
-    public final BoolSetting async = addSetting(new BoolSetting("Async", true));
     public final BoolSetting multitask = addSetting(new BoolSetting("Multitask", false));
     public final BoolSetting allowOffhand = addSetting(new BoolSetting("AllowOffhand", false));
     public final EnumSetting<EchestPriority> echestPriority = addSetting(new EnumSetting<>("Echest", EchestPriority.SILK));
@@ -77,6 +77,7 @@ public class SpeedMineFeature extends Feature {
         echestPriority.setShowCondition(()-> swap.get() != Swap.NONE);
         damageThreshold.setShowCondition(()-> swap.get() != Swap.NONE);
         allowOffhand.setShowCondition(()-> !multitask.get());
+        asyncRemine.setShowCondition(instant::get);
     }
 
     @Override
@@ -211,9 +212,11 @@ public class SpeedMineFeature extends Feature {
             if (instant.get()) {
                 task.markInstantRemine();
                 task.setProgress(1.0f);
-            } else
+            } else {
                 task.resetProgress();
-            return;
+            }
+            if (!asyncRemine.get())
+                return;
         }
 
         if (swing.get())
@@ -305,14 +308,14 @@ public class SpeedMineFeature extends Feature {
     }
 
     private void finishMining(BlockBreakingTask task) {
-        if (!task.isStarted() || task.getBlockState().isAir() && !async.get()) return;
+        if (!task.isStarted() || task.getBlockState().isAir() && !asyncRemine.get()) return;
         if (!multitask.get() && MC.player.isUsingItem()) {
             if (!(allowOffhand.get() && MC.player.getUsedItemHand() == InteractionHand.OFF_HAND)) { // yo somehow on some paper servers we can do it
                 return;
             }
         }
 
-        if (currentTask.lastBrokenCount == currentTask.brokenCount && !async.get())
+        if (currentTask.lastBrokenCount == currentTask.brokenCount && !asyncRemine.get())
             return;
 
         Vec3 eyePos = MC.player.getEyePosition();
